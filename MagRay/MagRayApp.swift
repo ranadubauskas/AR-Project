2bd2220d067477c63ea0776b72719a3d8a4db188
Rana Dubauskas <ranadubauskas@gmail.com>
2026-03-24 13:07:20 -0500

    Initial Commit

---
+++ MagRay/MagRayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagRayApp.swift
+//  MagRay
+//
+//  Created by Rana Dubauskas on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct MagRayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
